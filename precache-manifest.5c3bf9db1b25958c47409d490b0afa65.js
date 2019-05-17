b0c0f66a54ca989c177f42b7141d27d7f1171a54
sansil <sansil32@gmail.com>
2019-05-17 15:35:56 -0300

    deploy

---
+++ precache-manifest.5c3bf9db1b25958c47409d490b0afa65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/busqueda-del-tesoro-generator/robots.txt"
+  },
+  {
+    "revision": "8f41c43ebfa017d3a165",
+    "url": "/busqueda-del-tesoro-generator/js/chunk-vendors.ac21984e.js"
+  },
+  {
+    "revision": "6b6ce475c09544ccca5c",
+    "url": "/busqueda-del-tesoro-generator/js/app.e35e06da.js"
+  },
+  {
+    "revision": "ca0e43eb5074ef40ef53",
+    "url": "/busqueda-del-tesoro-generator/js/about.17b1acd1.js"
+  },
+  {
+    "revision": "14ec6631769ab2916c81d0ec3be76912",
+    "url": "/busqueda-del-tesoro-generator/index.html"
+  },
+  {
+    "revision": "6b6ce475c09544ccca5c",
+    "url": "/busqueda-del-tesoro-generator/css/app.43e07fd3.css"
+  }
+];
